95ee7fcfaaf6b454537e97174f47f08ff94c2f18
Irlanda <irlanda@iMac-de-Irlanda.local>
2022-09-21 20:37:00 -0600

    finished

---
+++ map.js
@@ -0,0 +1,26 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+var marker = L.marker([24.80011621443616, -107.34849863813737]).addTo(map);
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+var polygon = L.polygon([
+    [24.81329120891455, -107.43383549288852],
+    [24.814016111596796, -107.43215810179703],
+    [24.808913011763384, -107.42946888056568],
+    [24.80864956392223, -107.43162348769562]
+]).addTo(map);
+
+marker.bindPopup("<b>Hola profe!</b><br>Aquí tiene su casa.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("Aquí está la poderosísima <b>UAdeO<b>.");
